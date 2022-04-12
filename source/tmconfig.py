2e6f0b2beb28ab892dd54a7dec73c564660224cb
AkimBerreqia <berreqia.akim@gmail.com>
2022-04-12 10:04:35 +0000

    changement titre

--- source/tmconfig.py
+++ source/tmconfig.py
@@ -1,6 +1,6 @@
 class TMConfig:
 
-    title = u"Site interactif, apprenant la cryptologie"
+    title = u"Site interactif d'apprentissage de la cryptologie"
     first_name = 'Akim'
     last_name = 'Berreqia'
     author = f'{first_name} {last_name}'
@@ -8,7 +8,7 @@ class TMConfig:
     month = u'Décembre'
     seminary_title = u'Développement d’outils ou matériel d’enseignement de l’informatique'
     tutor = u"Cédric Donner"
-    release = "Version intermédiaire"
+    release = "Version finale"
     repository_url = "https://github.com/{your-docs-url}"
 
     @classmethod
